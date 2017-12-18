d9c056902c180b7ef2d6ab5d1efce11a94b753a1
bb s <424626154@qq.com>
2017-12-18 09:28:48 +0800

    调整banner系统

--- dao/bannerDao.js
+++ dao/bannerDao.js
@@ -2,16 +2,25 @@ var pool = require('./dao');
 var utils = require('../utils/utils');
 
 module.exports = {
-	addBanner:function(title,content,type,url,pid,callback){
+	addBanner:function(title,content,type,url,pid,author,callback){
 		var time = utils.getTime();
-		var sql = 'INSERT INTO banner (title,content,type,url,pid,time) VALUES (?,?,?,?,?,?)';       
+		var sql = 'INSERT INTO banner (title,content,type,url,pid,author,time) VALUES (?,?,?,?,?,?,?)';       
         pool.getConnection(function(err, connection) {
-            connection.query(sql,[title,content,type,url,pid,time], function(err, result) {
+            connection.query(sql,[title,content,type,url,pid,author,time], function(err, result) {
             	callback(err, result)
                 connection.release();
             });
         });
 	},
+    queryBanner:function(id,callback){
+        var sql = 'SELECT * FROM banner WHERE id = ? AND del = 0 ORDER BY id DESC';
+        pool.getConnection(function(err, connection) {
+            connection.query(sql, [id],function(err, result) {
+                callback(err, result)
+                connection.release();
+            });
+        });
+    },
 	queryBanners:function(callback){
 		var sql = 'SELECT * FROM banner WHERE del = 0 ORDER BY id DESC';
 		pool.getConnection(function(err, connection) {
@@ -25,6 +34,16 @@ module.exports = {
         var sql = 'UPDATE banner SET state = ? WHERE id = ? ';
         pool.getConnection(function(err, connection) {
             connection.query(sql, [state,id], function(err, result) {
+                callback(err, result)
+                connection.release();
+            });
+        });
+    },
+    upBanner:function(id,title,content,type,url,pid,author,callback){
+        var time = utils.getTime();
+        var sql = 'UPDATE banner SET title = ? , content = ? , type = ? , url = ? , pid = ? , author = ?, time = ? WHERE id = ? ';
+        pool.getConnection(function(err, connection) {
+            connection.query(sql,[title,content,type,url,pid,author,time,id], function(err, result) {
                 callback(err, result)
                 connection.release();
             });
--- routes/banner.js
+++ routes/banner.js
@@ -95,7 +95,7 @@ router.post('/addbanner',multipartMiddleware,function(req, res, next){
 		    res.render('addbanner', { user: user,err:err});
   		}else{
   			btype = 1;
-  			bannerDao.addBanner(title,content,btype,url,0,function(err,result){
+  			bannerDao.addBanner(title,content,btype,url,0,'',function(err,result){
   				if(err){
   					logger.error(err)
   					res.render('addbanner', { user: user,err:err});
@@ -111,7 +111,7 @@ router.post('/addbanner',multipartMiddleware,function(req, res, next){
 		    res.render('addbanner', { user: user,err:err});
   		}else{
   			btype = 2;
-  			bannerDao.addBanner(title,content,btype,'',pid,function(err,result){
+  			bannerDao.addBanner(title,content,btype,'',pid,'',function(err,result){
   				if(err){
   					logger.error(err)
   					res.render('addbanner', { user: user,err:err});
@@ -121,13 +121,13 @@ router.post('/addbanner',multipartMiddleware,function(req, res, next){
   			});
   		}
     }else if(type == 'famous'){
-      var pid = body.pid;
-      if(!pid){
-        err = "名家作品id错误!";
+      var author = body.author;
+      if(!author){
+        err = "作者名称错误!";
         res.render('addbanner', { user: user,err:err});
       }else{
         btype = 3;
-        bannerDao.addBanner(title,content,btype,'',pid,function(err,result){
+        bannerDao.addBanner(title,content,btype,'',0,author,function(err,result){
           if(err){
             logger.error(err)
             res.render('addbanner', { user: user,err:err});
@@ -140,6 +140,108 @@ router.post('/addbanner',multipartMiddleware,function(req, res, next){
   		err = '类型错误';
   		res.render('addbanner', { user: user,err:err});
   	}
+  }
+});
+/**
+ * 修改banner
+ */
+router.get('/upbanner', function(req, res, next) {
+  var user = req.cookies.user;
+  if(!user){
+    res.redirect("/login");
+    return ;
+  }
+  var id = req.query.id;
+  if(id){
+      bannerDao.queryBanner(id,function(err,result){
+        if(err){
+          res.render('upbanner', { user: user,err:err,obj:null});
+        }else{
+          if(result.length > 0){
+            res.render('upbanner', { user: user,err:null,obj:result[0]});
+          }else{
+            res.render('upbanner', { user: user,err:'ID错误',obj:null});
+          }
+        }
+      })
+  }else{
+      res.render('upbanner', { user: user,err:'参数错误',obj:null});
+  }
+});
+
+
+router.post('/upbanner',multipartMiddleware,function(req, res, next){
+  var user = req.cookies.user;
+  if(!user){
+    res.redirect("/login");
+    return ;
+  }
+  console.log('post addbanner');
+  // res.send('sendmsg');
+  console.log(req.body);
+  var body = req.body;
+  var id = body.id;
+  var title = body.title;
+  var content = body.content;
+  var type = body.type;
+  var err = "";
+  if(!title||!content||!type){
+    err = "参数错误!";
+    res.render('addbanner', { user: user,err:err});
+  }else{
+    var btype = 0;
+    if(type == 'web'){
+      var url = body.url;
+      if(!url){
+        err = "外链地址错误!";
+        res.render('addbanner', { user: user,err:err});
+      }else{
+        btype = 1;
+        bannerDao.upBanner(id,title,content,btype,url,0,'',function(err,result){
+          if(err){
+            logger.error(err)
+            res.render('addbanner', { user: user,err:err});
+          }else{
+            res.redirect("/banner");
+          }
+        });
+      }
+    }else if(type == 'poem'){
+      var pid = body.pid;
+      if(!pid){
+        err = "作品id错误!";
+        res.render('addbanner', { user: user,err:err});
+      }else{
+        btype = 2;
+        bannerDao.upBanner(id,title,content,btype,'',pid,'',function(err,result){
+          if(err){
+            logger.error(err)
+            res.render('addbanner', { user: user,err:err});
+          }else{
+            res.redirect("/banner");
+          }
+        });
+      }
+    }else if(type == 'famous'){
+      var author = body.author;
+      if(!author){
+        err = "名家作者名称错误!";
+        res.render('addbanner', { user: user,err:err});
+      }else{
+        btype = 3;
+        bannerDao.upBanner(id,title,content,btype,'',0,author,function(err,result){
+          if(err){
+            logger.error(err)
+            res.render('addbanner', { user: user,err:err});
+          }else{
+            res.redirect("/banner");
+          }
+        });
+      }
+    }else{
+      err = '类型错误';
+      res.render('addbanner', { user: user,err:err});
+    }
   }
 });
 
